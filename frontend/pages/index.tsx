7b4176d19dd9d501c635121ed0b02a38ef019fdb
Pauline Kraus <kraus.pauline1@gmail.com>
2023-02-01 16:09:44 +0100

    index page desktop done

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,11 @@ import { Inter } from "@next/font/google";
 import Header from "@/components/Header";
 import ButtonBlack from "@/components/ButtonInner";
 import Link from "next/link";
+import donutWhiteXL from "../assets/donut-white-XL.png";
+import {
+  MouseParallaxContainer,
+  MouseParallaxChild,
+} from "react-parallax-mouse";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,21 +15,38 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-[96px] font-bold">Hi!</h1>
-        <h1 className="text-[96px] font-bold">
-          I&apos;m <span className="text-[#0C26CD]">Pauline</span>
-        </h1>
-        <p className="text-[40px] w-[40%] text-center mb-[44px]">
-          A bilingual Frontend Web Developer keen on design.
-        </p>
-        <div className="flex items-center justify-center gap-[10%] w-full ">
-          <Link href="/Work">
-            <ButtonBlack label="See my projects" />
-          </Link>
-          <Link href="/About">
-            <ButtonBlack label="About me" />
-          </Link>
+      <div>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <h1 className="text-[96px] font-bold z-10 -mb-7">Hi!</h1>
+          <h1 className="text-[96px] font-bold z-10 mt-0">
+            I&apos;m <span className="text-[#0C26CD]">Pauline</span>
+          </h1>
+          <p className="text-[40px] w-[40%] text-center mb-[44px] z-10">
+            A bilingual Frontend Web Developer keen on design.
+          </p>
+          <div className="flex items-center justify-center gap-[10%] w-full z-10">
+            <Link href="/Work">
+              <ButtonBlack label="See my projects" />
+            </Link>
+            <Link href="/About">
+              <ButtonBlack label="About me" />
+            </Link>
+          </div>
+          <div className="flex items-center justify-center absolute z-1">
+            <MouseParallaxContainer
+              globalFactorX={0.7}
+              globalFactorY={0.7}
+              useWindowMouseEvents={true}
+            >
+              <MouseParallaxChild factorX={0.9} factorY={0.9}>
+                <Image
+                  src={donutWhiteXL}
+                  alt="donut White Background"
+                  className="w-[500px]"
+                />
+              </MouseParallaxChild>
+            </MouseParallaxContainer>
+          </div>
         </div>
       </div>
     </>
