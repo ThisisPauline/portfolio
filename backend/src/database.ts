9589dae52d077291c6370a7e25b6cca0a5e43cb1
Pauline Kraus <kraus.pauline1@gmail.com>
2023-03-09 18:59:03 +0100

    tsx changes and email address

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const database = mysql.createPool({
-host: process.env.DB_HOST, 
-port: parseInt(process.env.DB_PORT ?? "3306"),
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-});
+    host: process.env.DB_HOST, 
+    port: parseInt(process.env.PORT || process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
 
 export default database;
--- backend/src/projects/projectsHandlers.ts
+++ backend/src/projects/projectsHandlers.ts
@@ -2,7 +2,7 @@ import database from "../database"
 
 export const getAllProjects = (req: any, res: any) => {
   database
-    .query("SELECT * FROM projectSpecs INNER JOIN photos on projectSpecs.id = photos.`projectSpecs_id` where photos.name LIKE '%ONE'")
+    .execute("SELECT * FROM projectSpecs INNER JOIN photos on projectSpecs.id = photos.`projectSpecs_id` where photos.name LIKE ?", ['%ONE'])
     .then((result: any[]) => {
       res.status(200).json(result[0]);
     })
@@ -12,22 +12,20 @@ export const getAllProjects = (req: any, res: any) => {
     });
 };
 
-
-
 export const getProjectByID = (req:any, res:any) => {
   const projectID = req.params.id;
   database
-  .query("  SELECT * FROM photos INNER JOIN projectSpecs on projectSpecs.id = photos.`projectSpecs_id` WHERE projectSpecs.id = ?", [projectID])
+  .execute("SELECT * FROM photos INNER JOIN projectSpecs on projectSpecs.id = photos.`projectSpecs_id` WHERE projectSpecs.id = ?", [projectID])
   .then((result) => {
-  if ((result[0] as any).length === 0) {
-  res.status(404).send("Nope");
-  } else {
-  res.status(200).json(result[0]);
-  }
+    if ((result[0] as any).length === 0) {
+      res.status(404).send("Nope");
+    } else {
+      res.status(200).json(result[0]);
+    }
   })
   .catch((err) => {
-  console.error(err);
-  res.status(500).send("Internal Server Error");
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   });
-  };
+};
 
