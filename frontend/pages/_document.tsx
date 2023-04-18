07d0d7b7dbe1e19ffa76e8b1f657b6e1472cb902
Pauline Kraus <kraus.pauline1@gmail.com>
2023-04-18 14:22:34 +0200

    head tag

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:image" content="https://i.imgur.com/yCR2tJD.jpg" />
+        <meta property="og:title" content="Pauline | Frontend developer" />
+        <meta
+          property="og:description"
+          content="Hi, I'm Pauline, a frontend developer with a passion for pixel perfect design. Check my work out! "
+        />
       </Head>
       <body>
         <Main />
