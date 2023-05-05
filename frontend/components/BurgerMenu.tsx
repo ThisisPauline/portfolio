80b5c4b881dcce5378e0016093614862161096e3
Pauline Kraus <kraus.pauline1@gmail.com>
2023-05-05 18:16:04 +0200

    links

--- frontend/components/BurgerMenu.tsx
+++ frontend/components/BurgerMenu.tsx
@@ -69,7 +69,7 @@ export default function BurgerMenu(props: MenuPropTypes) {
                 </div>
                 <div className={styles.linkWhite}>
                   <Link
-                    href="/about"
+                    href="/About"
                     className="flex items-baseline"
                     onClick={handleOnClick}
                   >
@@ -81,7 +81,7 @@ export default function BurgerMenu(props: MenuPropTypes) {
                 </div>
                 <div className={styles.linkWhite}>
                   <Link
-                    href="/contact"
+                    href="/Contact"
                     className="flex items-baseline"
                     onClick={handleOnClick}
                   >
