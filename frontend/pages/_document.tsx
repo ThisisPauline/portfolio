de24576c70dcaa13285b926d4ca30585ba80ac91
Pauline Kraus <kraus.pauline1@gmail.com>
2023-05-06 10:09:06 +0200

    document head

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,25 +9,6 @@ export default function Document() {
           rel="stylesheet"
         />
 
-        <meta
-          name="keywords"
-          content="pauline kraus web developer contact, pauline kraus dev contact, pauline kraus frontend"
-        ></meta>
-        <meta
-          name="description"
-          content="Hi, I'm Pauline, a frontend developer with a passion for pixel perfect designs. Check my work out! "
-        ></meta>
-        <meta
-          property="og:title"
-          content="Pauline kraus | Frontend developer"
-        />
-        <meta property="og:type" content="" />
-        <meta property="og:url" content="" />
-        <meta property="og:image" content="https://i.imgur.com/yCR2tJD.jpg" />
-        <meta
-          property="og:description"
-          content="Hi, I'm Pauline, a frontend dev with a passion for pixel-perfect designs. Check out my work!"
-        />
         <meta charSet="utf-8"></meta>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
